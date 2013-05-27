6aa43b978e2e7ad3920ac0328c7f78ed58a1b1f7
Yasuhiro Matsuda <ysmazda@gmail.com>
2013-05-26 19:22:56 -0700

    Change the JS framework to AngularJS.

--- client/main.js
+++ client/main.js
@@ -1,90 +1,16 @@
-$(function(){
-
-  window.Book = Backbone.Model.extend({
-    clear: function() {
-      this.destroy();
-      this.view.remove();
-    }
+angular.module('bookServices', ['ngResource']).
+    factory('Book', function($resource){
+  return $resource('books/:bookId', {}, {
+    query: {method:'GET', params:{bookId: ''}, isArray:true}
   });
-
-  window.BookList = Backbone.Collection.extend({
-    model: Book,
-
-    url: '/books',
-
-    parse: function(response) {
-      var books = response;
-      for (var i = 0; i < books.length; ++i) {
-        this.add(new Book(books[i]));
-      }
-    },
-  });
-
-  window.BookView = Backbone.View.extend({
-    tagName: 'tr',
-    template: _.template($('#book-template').html()),
-
-    initialize: function() {
-      _.bindAll('render');
-      this.model.view = this;
-    },
-
-    render: function() {
-      $(this.el).html(this.template(this.model.toJSON()));
-      return this;
-    }
-  });
-
-  window.booklist = new BookList;
-
-  // The Application
-  // ---------------
-
-  window.AppView = Backbone.View.extend({
-
-    el: $("#booktracker-app"),
-
-    events: {
-      'click #delete': 'deleteBooks',
-      'click #edit': 'editBooks',
-      'click #update': 'updateBooklist',
-    },
-
-    initialize: function() {
-      _.bindAll(this, 'addBook', 'deleteBooks', 'editBooks', 'removeBookDOM',
-                'updateBooklist');
-      booklist.bind('add', this.addBook)
-      booklist.fetch();
-    },
-
-    addBook: function(book) {
-      var view = new BookView({model: book});
-      this.$('#booklist-table-body').append(view.render().el);
-    },
-
-    deleteBooks: function() {
-      var self = this;
-      $.get('/delete', function() {
-        self.removeBookDOM();
-      });
-    },
-
-    editBooks: function() {
-      location.href = '/edit';
-    },
-
-    removeBookDOM: function () {
-      this.$('#booklist tr td').parent().remove();
-    },
-
-    updateBooklist: function() {
-      var self = this;
-      $.get('/update', function() {
-        self.removeBookDOM();
-        booklist.fetch();
-      });
-    }
-  });
-
-  window.App = new AppView;
 });
+
+angular.module('booktracker', ['bookServices']).
+  config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+      otherwise({redirectTo: '/'});
+}]);
+
+function BooktrackerCtrl($scope, Book) {
+  $scope.books = Book.query();
+}
